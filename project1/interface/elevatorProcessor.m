710fcedb9c570357719caf7f3c9fc125e084abcd
Weichen Lu <luwch@shanghaitech.edu.cn>
2020-05-15 20:06:08 +0800

    p3 report

--- project1/interface/elevatorProcessor.m
+++ project1/interface/elevatorProcessor.m
@@ -1,10 +1,30 @@
 classdef elevatorProcessor < handle
     properties
+        ElevatorApp
+        FloorApp
         leftElevator
         rightElevator
     end
     
     methods
+        function createLeftElevator(process, speed, acceleration, doorkeeptime, limittime)
+            left = singleElevator;
+            left.speed = speed;
+            left.acceleration = acceleration;
+            left.doorkeeptime = doorkeeptime;
+            left.limittime = limittime;
+            process.leftElevator = left;
+        end
+        
+        function createRightElevator(process, speed, acceleration, doorkeeptime, limittime)
+            right = singleElevator;
+            right.speed = speed;
+            right.acceleration = acceleration;
+            right.doorkeeptime = doorkeeptime;
+            right.limittime = limittime;
+            process.rightElevator = right;
+        end
+        
         function El = chooseElevator(process, direction, currentFloor)
            El = 0; % 0 means choose the left elevator, 1 means choose the right elevator 
            if process.leftElevator.currentFloor == currentFloor
--- project1/interface/singleElevator.m
+++ project1/interface/singleElevator.m
@@ -2,8 +2,8 @@ classdef singleElevator < handle
    properties
        speed            % the max speed of the elevator
        acceleration     % the acceleration
-       direction        % up(1) or down(-1)
-       currentFloor     % simply, which floor the elevator is at.
+       direction        % up(1) or down(-1), if no command, be zero
+       currentFloor = 1;% simply, which floor the elevator is at. Initially at 1.
        defaultFloor = 1;
        doorKeepTime     % after open the door, keep the door open for a short period of time
        limitTime        % time keep in the floor after finally usage before let it go to default floor
