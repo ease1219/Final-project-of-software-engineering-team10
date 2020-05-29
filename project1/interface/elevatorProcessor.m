93da7ffa4dd9f7074f70991bd2415395884b1592
Weichen Lu <luwch@shanghaitech.edu.cn>
2020-05-29 22:04:50 +0800

    p1 code update2

--- project1/interface/elevatorProcessor.m
+++ project1/interface/elevatorProcessor.m
@@ -7,6 +7,8 @@ classdef elevatorProcessor < handle
         MiddleFloorApp      % UI
         leftElevator
         rightElevator
+        tempUp = [];
+        tempDown = [];
     end
     
     methods
@@ -28,8 +30,60 @@ classdef elevatorProcessor < handle
             process.rightElevator = right;
         end
         
+        function addUpRequest(process,aim)
+           if not(any(process.tempUp==aim))
+               process.tempUp = sort([process.upRequest,aim]);
+           end
+        end
+       
+        function addDownRequest(process,aim)
+            if not(any(process.downRequest==aim))
+                process.downRequest = sort([process.downRequest,aim],'descent');
+            end
+        end
+        
+        function judgeDist(process,floor)   % add the request to the nearest elevator
+            if abs(process.leftElevator.currentFloor - floor) < abs(process.rightElevator.currentFloor - floor)
+                process.leftElevator.receiveReq(process,floor);
+            else
+                process.rightElevator.receiveReq(process,floor);
+            end
+        end
+        
         function receiveReq(process, floor, direction)
-            
+            if process.leftElevator.direction == 0 && process.rightElevator.direction == 0
+                process.judgeDist(process,floor);
+                
+            elseif process.leftElevator.direction == 0 || process.rightElevator.direction == 0
+                if process.leftElevator.direction == 0 
+                    if process.leftElevator.currentFloor == floor
+                        process.leftElevator.receiveReq(process,floor);
+                    elseif process.rightElevator.direction == direction
+                        process.rightElevator.receiveReq(process,floor);
+                    else 
+                        process.judgeDist(process,floor);
+                    end
+                    
+                else
+                    if process.rightElevator.currentFloor == floor
+                        process.rightElevator.receiveReq(process,floor);
+                    elseif process.leftElevator.direction == direction
+                        process.leftElevator.receiveReq(process,floor);
+                    else 
+                        process.judgeDist(process,floor);
+                    end
+                end
+            else 
+                if process.leftElevator.direction == direction && process.rightElevator.direction == direction
+                    process.judge(process,floor);
+                elseif process.leftElevator.direction == direction 
+                    process.leftElevator.receiveReq(process,floor);
+                elseif process.rightElevator.direction == direction
+                    process.rightElevator.receiveReq(process,floor);
+                else
+                    if direction
+                end
+            end
         end
     end
     
